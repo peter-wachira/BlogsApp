b0f6e85fd7715972b616ca4ce0ad73cf9d0f9fa0
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-17 14:31:53 +0300

    modify base use case interface

--- app/src/main/java/com/droid/diexample/domain/usecases/BaseUseCase.kt
+++ app/src/main/java/com/droid/diexample/domain/usecases/BaseUseCase.kt
@@ -1,4 +1,5 @@
 package com.droid.diexample.domain.usecases
 
-interface BaseUseCase {
+interface BaseUseCase<in Parameter,out Result> {
+    suspend operator fun invoke(parameter: Parameter):Result
 }
