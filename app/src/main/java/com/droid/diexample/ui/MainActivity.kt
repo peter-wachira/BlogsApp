f847cd5bab5fd3efcf1876bdeeba6c6f74ae2c4b
PeteHack <pwachira900@gmail.com>
2020-12-17 08:39:04 +0300

    add methods to display data & loaders

--- app/src/main/java/com/droid/diexample/ui/MainActivity.kt
+++ app/src/main/java/com/droid/diexample/ui/MainActivity.kt
@@ -1,25 +1,66 @@
-package com.droid.diexample
+package com.droid.diexample.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Message
+import android.view.View
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import com.droid.diexample.R
-import dagger.Binds
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
+import com.droid.diexample.model.Blog
+import com.droid.diexample.util.DataState
+
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.components.ActivityRetainedComponent
-import dagger.hilt.android.components.ApplicationComponent
-import org.intellij.lang.annotations.PrintFormat
-import javax.inject.Inject
-import javax.inject.Singleton
+import kotlinx.android.synthetic.main.fragment_main.*
+import java.lang.StringBuilder
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private  val TAG = "AppDebug"
+    private  val viewModel : MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        subscribeObservers()
+        viewModel.setStateEvent(MainStateEvent.GetBlogsEvent)
     }
+
+
+    private fun subscribeObservers(){
+        viewModel.dataState.observe(this, Observer { dataState ->
+            when(dataState){
+               is DataState.Success<List<Blog>> ->{
+                    displayProgressBar(false)
+                   appendBlogTitles(dataState.data)
+               }
+               is DataState.Error ->{
+                    displayProgressBar(false)
+                   dataState.exception.message?.let { displayError(it) }
+               }
+               is DataState.Loading ->{
+                   displayProgressBar(true)
+               }
+            }
+        })
+    }
+
+    private fun displayError(message: String){
+        if (message != null) text.text = message else text.text = "Unknown error."
+    }
+
+    private fun appendBlogTitles(blogs: List<Blog>){
+        val sb = StringBuilder()
+        for(blog in  blogs){
+            sb.append(blog.title + "\n");
+        }
+        text.text = sb.toString()
+    }
+
+    private fun displayProgressBar(isDisplayed: Boolean){
+        progress_bar.visibility = if(isDisplayed) View.VISIBLE else View.GONE
+    }
+
+
 }
 
