65c14309f6860c380594563af886c67bd970c1ce
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-17 14:38:55 +0300

    modify blogs use case

--- app/src/main/java/com/droid/diexample/domain/usecases/BlogsUseCase.kt
+++ app/src/main/java/com/droid/diexample/domain/usecases/BlogsUseCase.kt
@@ -1,5 +1,15 @@
 package com.droid.diexample.domain.usecases
 
-class BlogsUseCase {
+import com.droid.diexample.domain.BlogDomain
+import com.droid.diexample.domain.repository.IBlogsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+typealias BlogsBaseUseCase = BaseUseCase<Unit, Flow<List<BlogDomain>>>
+
+class BlogsUseCase(private val repository: IBlogsRepository) : BlogsBaseUseCase {
+    override suspend fun invoke(parameter: Unit): Flow<List<BlogDomain>> =
+        repository.getBlogs()
+
 
 }
