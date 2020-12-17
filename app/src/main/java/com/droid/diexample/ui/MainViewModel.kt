0f56d3265a82d21d0c4f3934c3357d4146be2ab0
PeteHack <pwachira900@gmail.com>
2020-12-17 08:36:17 +0300

    add main view model

---
+++ app/src/main/java/com/droid/diexample/ui/MainViewModel.kt
@@ -0,0 +1,55 @@
+package com.droid.diexample.ui
+
+import androidx.hilt.Assisted
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.*
+import com.droid.diexample.model.Blog
+import com.droid.diexample.repository.MainRepository
+import com.droid.diexample.util.DataState
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+
+
+@ExperimentalCoroutinesApi
+class MainViewModel
+@ViewModelInject
+constructor(
+        private val mainRepository: MainRepository,
+        @Assisted private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private val _dataState: MutableLiveData<DataState<List<Blog>>> = MutableLiveData()
+
+    val dataState: LiveData<DataState<List<Blog>>>
+        get() = _dataState
+
+    fun setStateEvent(mainStateEvent: MainStateEvent) {
+        viewModelScope.launch {
+            when (mainStateEvent) {
+                    is MainStateEvent.GetBlogsEvent -> {
+                            mainRepository.getBlogs()
+                                    .onEach { dataState ->
+                                            _dataState.value = dataState
+                                    }
+                                    .launchIn(viewModelScope)
+                    }
+
+                    is MainStateEvent.None -> {
+                            // who cares
+                    }
+            }
+        }
+    }
+
+}
+
+
+sealed class MainStateEvent {
+
+    object GetBlogsEvent : MainStateEvent()
+
+    object None : MainStateEvent()
+}
+
